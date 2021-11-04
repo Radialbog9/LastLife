a3a0c6d878bc7151c6493fdbaecbf17dcea7bbcb
TheJoeCoder <joe@radialbog9.uk>
2021-11-04 19:25:37 +0000

    Initial plugin template

---
+++ src/main/java/uk/radialbog9/spigot/lastlife/LastLife.java
@@ -0,0 +1,17 @@
+package uk.radialbog9.spigot.lastlife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LastLife extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
